78756aa337c67435c2b959628ddb80e7ca28694b
NikoZK <nikodem09@live.dk>
2026-01-29 10:33:08 +0100

    Variables with notes added

---
+++ 01._Miscellaneous/variables_1.js
@@ -0,0 +1,28 @@
+//Node REPL
+
+//type coercion
+//strict equality
+
+//Rule 1: use const per default unless it needs to change, then use let
+
+const luckyNumber = 10;
+
+//const is constant in the assignment 
+
+console.log(luckyNumber)
+
+//JS Data types
+//String, Number, BigInt, Boolean, Object, Symbol, null, undefined
+
+const person = {
+    name: "Niko" //key-value pair
+};
+
+person.name = "NikoZK"
+person.age = 24
+
+console.log(person)
+
+const people = []
+people.push("Victor") //Add to list
+console.log(people)
