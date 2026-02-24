c7bbfb9c1da2ed6e91c8645b1f4bc313465bb5b5
NikoZK <nikodem09@live.dk>
2026-02-24 13:36:32 +0100

    Finished all functionality for now

--- 05.HTML_Server/App.js
+++ 05.HTML_Server/App.js
@@ -6,6 +6,28 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/frontpage.html')
 })
 
+/*
+            async function randomTimeGenerator() {
+                const response = await fetch('/random-time')
+                const data = await response.json()
+            if (data.message) {
+                randomTimeDisplay.innerText = data.message
+            } else {
+                randomTimeDisplay.innerText = "No do it in " + timeFormat(data.seconds)
+            }
+        }
+
+
+        app.get('/random-time', (req, res) => {
+            let yesOrTime = Math.random()
+            if (yesOrTime > 0.5) {
+                res.send({message: "Yes"})
+            } else {
+                let totalSeconds = Math.floor(Math.random() * (129600 - 1) + 1)
+                res.send({seconds: totalSeconds})
+            }
+        })
+*/
 
 app.listen(8080, (error) => {
     if (error) {
